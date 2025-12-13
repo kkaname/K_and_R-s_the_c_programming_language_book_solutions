184ac1089916526906d352c4985b0ce1221397e4
Ayush Hegde <ayushhegde2005@gmail.com>
2025-12-14 04:33:57 +0530

    exercise 1.11

---
+++ chapter_1/exercise_1_11/word_count.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define IN 1    //represents inside a word
+#define OUT 0   //represents outside a word
+
+int main(void) {
+    int c, state;
+    int word_count;
+
+    word_count = 0;
+    state = OUT;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n') {
+            state = OUT;
+        }
+        else if (state == OUT) {
+            ++word_count;
+            state = IN;
+        }
+    }
+
+    printf("Number of words: %d\n", word_count);
+
+    return 0;
+}
