9257d22a65ec67e2468387f58a453bbda8bcd561
Ayush Hegde <ayushhegde2005@gmail.com>
2025-12-14 04:22:36 +0530

    exercise 1.8

---
+++ chapter_1/exercise_1_08/count_b_t_n.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* Exercise 1-8: Write a program to count blanks, tabs and newlines. */
+
+int main(void) {
+    int num_blank, num_tab, num_newline, c;
+
+    num_blank = num_tab = num_newline = 0;
+
+    while((c = getchar()) != EOF) {
+        if(c == '\n') {
+            ++num_newline;
+        }
+
+        if(c == '\t') {
+            ++num_tab;
+        }
+
+        if(c == ' ') {
+            ++num_blank;
+        }
+    }
+
+    printf("\nnumber of blank lines:%d\nnumber of tabs:%d\nnumber of newlines:%d\n", num_blank, num_tab, num_newline);
+    
+    return 0;
+}
