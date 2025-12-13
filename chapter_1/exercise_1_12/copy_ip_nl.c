aa058c1f7e41f964363d6e2cbfac76182f226e67
Ayush Hegde <ayushhegde2005@gmail.com>
2025-12-14 04:36:36 +0530

    exercise 1.12

---
+++ chapter_1/exercise_1_12/copy_ip_nl.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* Exercise 1.12: Write a program that prints its input one word per line. */
+
+#define IN 1
+#define OUT 0
+
+int main(void) {
+    int c, state;
+
+    state = OUT;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n') {
+            if (state = IN) {
+                putchar('\n');
+            }
+        }
+        else {
+            state = IN;
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
