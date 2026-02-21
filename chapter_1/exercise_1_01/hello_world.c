1ae52a9d9d31be5d6c2fe0809ed6842e6dc29fdb
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-21 21:41:13 +0530

    fix: removed the comment of the question and made readme file the question
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_1/exercise_1_01/hello_world.c
+++ chapter_1/exercise_1_01/hello_world.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-/* Exercise 1-2: Run the "hello, world" program on your system.
- * Expreiment with leaving out parts of the program, to see what error messages you get. */
-
 int main(void) {
     printf("hello, world\n");
 
