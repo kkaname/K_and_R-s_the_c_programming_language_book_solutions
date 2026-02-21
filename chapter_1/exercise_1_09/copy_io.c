48028a71dbb5cfdaa5cd51014ee16a5bc147e7d7
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-22 01:31:27 +0530

    fix: removed the question writtin as a comment and also formatted the code
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_1/exercise_1_09/copy_io.c
+++ chapter_1/exercise_1_09/copy_io.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-/* Exercise 1.09: Write a program to copy its input to its output,
- * replacing each string of one or more blanks by a single blank. */
-
 int main(void) {
     int c;
-    int last_c = '\0';
+    int last_c = 0;
 
-    while((c = getchar()) != EOF) {
-        if(c != ' ' || last_c != ' ') {
+    while ((c = getchar()) != EOF) {
+        if (!(c == ' ' && last_c == ' ')) {
             putchar(c);
         }
 
-        last_c = c;    //stores the character for comparision with the next character.
+        last_c = c;    // save current character to detect consecutive spaces
     }
 
     return 0;
