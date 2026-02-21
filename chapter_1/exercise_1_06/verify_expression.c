fb6b698ae699cdca094fa335d591680d2896a978
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-22 00:20:24 +0530

    fix: removed the comment about the question of the code problem
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_1/exercise_1_06/verify_expression.c
+++ chapter_1/exercise_1_06/verify_expression.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-/* Exercise 1-6: Verify that the expression getchar() != EOF is 0 or 1. */
-
 int main(void) {
     printf("The expression getchar() != EOF gives: %d\n", getchar() != EOF);
 
