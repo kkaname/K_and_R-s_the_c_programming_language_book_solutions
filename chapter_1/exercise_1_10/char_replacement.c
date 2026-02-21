49735d448316c94067f8e76e4bfba2c7473c234f
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-22 01:45:10 +0530

    fix: changed the program name to something simpler and also removed the program question written as comment
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_1/exercise_1_10/char_replacement.c
+++ chapter_1/exercise_1_10/char_replacement.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
-/* Exercise 1.10: Write a program to copy its input to its output, 
- * replacing each tab by \t, each backspace by \b, and each backslash by \\.
- * This makes tabs and backspaces visible in an unambiguous way. */
-
 int main(void) {
     int c;
 
     while((c = getchar()) != EOF) {
-        if (c == ' ') {
+        if (c == '\b') {
             putchar('\\');
             putchar('b');
         }
