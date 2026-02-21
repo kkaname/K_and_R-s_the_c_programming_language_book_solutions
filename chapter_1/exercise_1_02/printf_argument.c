f558f0335916405d7005e3416d9da929293ac335
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-21 22:33:47 +0530

    fix: removed the question as a comment as it was already in the readme file
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_1/exercise_1_02/printf_argument.c
+++ chapter_1/exercise_1_02/printf_argument.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-/* Exercise 1-2: Experiment to find out what happens when printf's argument string contains \c,
- * where c is some character not listed as part of the escape sequence. */
-
 int main(void) {
     printf("hello, world\c");   /*prints-> warning: unknown escape sequence '\c' */
 
