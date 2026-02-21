f203ffb2d736cf8295a70b06175674b45fd3e13d
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-21 23:28:45 +0530

    fix: corrected the formula which i had written wrong unknowingly
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_1/exercise_1_04/celcius_fahrenheit.c
+++ chapter_1/exercise_1_04/celcius_fahrenheit.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-/* Exercise 1-4: Write a program to print the corresponding celsius to fahrenheit table. */
-
 int main(void) {
-    float fahr, celcius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
@@ -13,11 +11,11 @@ int main(void) {
     printf("Celsius\t\tFahrenheit\n");
     printf("--------------------------\n");
 
-    celcius = lower;
-    while(celcius <= upper) {
-        fahr = (9.0 / 5.0) * celcius - 32.0f;
-        printf("%3.0f\t\t%6.2f\n", celcius, fahr);
-        celcius += step;
+    celsius = lower;
+    while(celsius <= upper) {
+        fahr = (9.0 / 5.0) * celsius + 32.0;
+        printf("%3.0f\t\t%6.2f\n", celsius, fahr);
+        celsius += step;
     }
 
     return 0;
