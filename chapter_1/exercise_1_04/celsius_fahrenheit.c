96bf8a89547549a742ab378801cbb323cea21a56
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-24 03:31:41 +0530

    fix: formatting of the loop
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_1/exercise_1_04/celsius_fahrenheit.c
+++ chapter_1/exercise_1_04/celsius_fahrenheit.c
@@ -12,7 +12,7 @@ int main(void) {
     printf("--------------------------\n");
 
     celsius = lower;
-    while(celsius <= upper) {
+    while (celsius <= upper) {
         fahr = (9.0 / 5.0) * celsius + 32.0;
         printf("%3.0f\t\t%6.2f\n", celsius, fahr);
         celsius += step;
