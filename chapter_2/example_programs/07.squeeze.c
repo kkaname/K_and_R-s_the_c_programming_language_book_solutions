178a8f080c4c734f91482c72cce9d43936f04231
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-23 11:17:18 +0530

    fix: changed the indexing number and also removed page number comment
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_2/example_programs/07.squeeze.c
+++ chapter_2/example_programs/07.squeeze.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /*
- * Example program in the page 42
  * program that removes the occurance of the character c from the string s
  */
 
 void squeeze(char s[], int c) {
     int i, j;
     for (i = j = 0; s[i] != '\0'; i++) {
-        if (s[i] != c && s[i] != c - 32) {
+        if (s[i] != c) {
             s[j++] = s[i];
         }
     }
